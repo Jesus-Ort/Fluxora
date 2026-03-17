f2612d71f0260f89b128b23d4ff9e90137e843a1
Jesus Ortega <xJesus_Nevermor3@hotmail.com>
2026-03-17 17:11:05 -0400

    Frontend: *Fixed table

--- Frontend/app/pages/categories.vue
+++ Frontend/app/pages/categories.vue
@@ -61,7 +61,10 @@ const columns: TableColumn<Category>[] = [
   {
     accessorKey: 'type',
     header: 'Tipo',
-    cell: ({ row }) => (row.type === 'Income' ? 'Gasto' : 'Ingreso' ),
+    cell: ({ row }) => {
+      const type = row.getValue('type') as string || row.original?.type
+      return type === 'Income' ? 'Ingreso' : 'Gasto'
+    },
   },
   {
     accessorKey: 'name',
